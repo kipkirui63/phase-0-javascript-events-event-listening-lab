4fe55901c4dd7fa6afc84e0732198311c4e30abb
kipkirui63 <kiprutonyachire@gmail.com>
2023-06-10 15:59:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
 }
+
+
+function addingEventListener() {
+  const input = document.getElementById('input');
+  
+  input.addEventListener('click', function() {
+    // Do something when the input element is clicked
+  });
+  
+  return true; // Indicate that the event listener was successfully added
+}
+
